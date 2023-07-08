0362a2a0c76d4185c2fc36396fa54159b8bccff0
Santiago S. Foschiatti <sanfosx@gmail.com>
2023-07-08 14:51:06 -0300

    [FEAT]-Primera version de la Navbar

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,9 +1,21 @@
-import React from 'react'
 
-const Navbar = () => {
+import { Link } from 'react-router-dom'
+function Navbar() {
   return (
-    <div>
-      <h1>Navbar</h1>
+    <div className="navbar navbar-container">
+      <div className="logo-content">
+        <Link to='./'>
+          <img src='../../../public/img/E-commerce.jpg' alt="" className="logo-img" />
+        </Link>
+      </div>
+
+      <ul className="content-links">
+        <li><Link to="./categories">Categorias</Link></li>
+        <li><Link to='./products'>Productos</Link></li>
+        <li><Link to='./users'>Usuarios</Link></li>
+        <li><Link to='./login'>Login</Link></li>
+      </ul>
+
     </div>
   )
 }
