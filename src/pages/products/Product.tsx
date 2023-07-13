d2b6994cab26aa9b504ade627c4b9c1a064ee386
Santiago S. Foschiatti <sanfosx@gmail.com>
2023-07-13 16:26:16 -0300

    creo la rama de la etapa2

--- src/pages/products/Product.tsx
+++ src/pages/products/Product.tsx
@@ -6,7 +6,7 @@ const Product = ({product}) => {
 
   return (
     <div>
-      <h2>{product.name}</h2>
+      <h2>{product.title}</h2>
       <h3>{product.price}</h3>
       <p>{product.description}</p>
       <Link to={`/products/${product.id}`}> Mas detalles</Link>
