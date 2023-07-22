5cbe233fcbd5282886e77025cfaece06636ce3dc
Santiago S. Foschiatti <sanfosx@gmail.com>
2023-07-22 17:53:27 -0300

    [FEAT]- Agrgo la vista para los botones de editar y eliminar [CSS]-agrego el css de las vistas

--- src/pages/categories/Category.tsx
+++ src/pages/categories/Category.tsx
@@ -1,14 +1,23 @@
 
 import { Link } from 'react-router-dom'
 
+import { BsFillTrash3Fill, BsPencilSquare } from "react-icons/bs";
+
+
 const Category = ({ category }) => {
   return (
     < >
       <img className=" category-img" src={category.image} alt="imagen de la categoria" />
 
+
+     
       <div className="card-content-category-list">
         <h1 className="card-title-categpory-list">{category.name}</h1>
         <Link to={`/categories/${category.id}`} state={{ categoryName: category.name }}> Ver productos</Link>
+        <div className="card-category-actions">
+        <Link className="category-icon"><BsPencilSquare/></Link>
+        <Link className="category-icon"><BsFillTrash3Fill/></Link>
+      </div>
       </div>
 
 
--- src/pages/products/Product.tsx
+++ src/pages/products/Product.tsx
@@ -1,5 +1,6 @@
 
 import { Link } from 'react-router-dom'
+import { BsFillTrash3Fill, BsPencilSquare } from "react-icons/bs";
 import './Products.css'
 
 const Product = ({ product }) => {
@@ -9,6 +10,10 @@ const Product = ({ product }) => {
     <div className="card-product-container">
       <img className="product-img" src={product.images[0]} alt=""></img>
       <div className="">
+        <div className="card-product-actions">
+          <Link className="product-icon"> <BsPencilSquare/></Link>
+          <Link className="product-icon"><BsFillTrash3Fill/></Link>
+        </div>
         <h2 className="card-product-title">{product.title}</h2>
         <p className="product-description">{product.description}</p>
         <Link to={`/products/${product.id}`}> Mas detalles</Link>
